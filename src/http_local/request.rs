efe23f52c8b446498b92a739ee8820b6e16af5c3
Raamesh <raamesh_bharadwaj@outlook.com>
2024-08-28 23:26:24 +0530

    [r2@] "Implement conversion from byte to Request type"

--- src/http_local/request.rs
+++ src/http_local/request.rs
@@ -1,7 +1,18 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
     query_params: Option<String>,
     method: Method
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!();
+    }
+}
+
+
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,9 @@
-use std::net::TcpListener;
+use std::{net::{TcpListener, TcpStream}, os::unix::net::SocketAddr};
+use std::io::Read;
+use crate::http_local::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
 
 pub struct Server {
     addr: String
@@ -15,5 +20,35 @@ impl Server {
         println!("Listening to connection requests on {}", self.addr);
 
         let listener: TcpListener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+
+            match listener.accept() {
+                Ok((mut stream, addr)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Recieved Request as {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) =>{
+
+                                },
+                                Err(e) => println!("Failed to parse Error: {}", e)
+                            }
+
+                        }, 
+                        Err(e) => {
+                            println!("Error Occured in reading the request to buffer: {}", e);
+                        }
+                    }
+
+                },
+                Err(e) => {
+                    println!("Stream Establishment failed with Error: {}", e)
+                }
+                
+            }
+        }
     }
 }
