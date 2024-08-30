e4c4b263be68772aae2be9ed6afc8c1d5c1322fb
Raamesh <raamesh_bharadwaj@outlook.com>
2024-08-30 23:22:00 +0530

    [r2@] "Implementing Error types"

--- src/http_local/request.rs
+++ src/http_local/request.rs
@@ -1,5 +1,7 @@
 use super::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Display, Debug, Result as fmtResult, Formatter};
 
 pub struct Request {
     path: String,
@@ -8,11 +10,45 @@ pub struct Request {
 }
 
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         unimplemented!();
     }
 }
 
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
 
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol", 
+            Self::InvalidMethod => "Invalid Method"
+            
+        }
+    }
+}
+
+impl Error for ParseError {
+    
+}
